5652b9433593039b1ea58da1c6a1c25b1ceb686c
chintamanichavan <chintamani.chavan97@gmail.com>
2021-02-24 07:19:59 -0500

    ValidPalindrome Java

--- Java/Algorithms and Data Structures/String Algorithms/ValidPalindrome.java
+++ Java/Algorithms and Data Structures/String Algorithms/ValidPalindrome.java
@@ -1,3 +1,20 @@
 public class ValidPalindrome {
-    
+    public static boolean validPalindrome(String args) {
+        char strLetter = (char)0;
+        StringBuilder sb = new StringBuilder();
+        String reverseOut = "";
+        for(int i= args.length() - 1;i >= 0;i--){
+            strLetter = args.charAt(i);
+            reverseOut = sb.append(strLetter).toString();
+        }
+        if ( args.equals(reverseOut)){
+            System.out.println("It is a valid PalinDrome.");
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String input = "dad";
+        validPalindrome(input);
+    }
 }
