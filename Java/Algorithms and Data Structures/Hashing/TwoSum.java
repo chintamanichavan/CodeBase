401db4f97227cbcadba23c6297a49f2d14e331a0
chintamanichavan <chintamani.chavan97@gmail.com>
2021-02-24 07:14:39 -0500

    ReverseString Java

--- Java/Algorithms and Data Structures/Hashing/TwoSum.java
+++ Java/Algorithms and Data Structures/Hashing/TwoSum.java
@@ -23,3 +23,44 @@ public class TwoSum {
         twoSum(nums, target);
     }
 }
+
+/*
+Two Sum
+Given an array of integers, return the indices of the two numbers whose sum is equal to a given target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Solution:
+METHOD 1. Naive approach: Use two for loops
+
+The naive approach is to just use two nested for loops and check if the sum of any two elements in the array is equal to the given target.
+
+Time complexity: O(n^2)
+
+METHOD 2. Use a HashMap (Most efficient)
+
+You can use a HashMap to solve the problem in O(n) time complexity. Here are the steps:
+
+    Initialize an empty HashMap.
+    Iterate over the elements of the array.
+    For every element in the array -
+        If the element exists in the Map, then check if it’s complement (target - element) also exists in the Map or not. If the complement exists then return the indices of the current element and the complement.
+        Otherwise, put the element in the Map, and move to the next iteration.
+
+Time complexity: O(n)
+
+METHOD 3. Use Sorting along with the two-pointer sliding window approach
+
+There is another approach which works when you need to return the numbers instead of their indexes. Here is how it works:
+
+    Sort the array.
+    Initialize two variables, one pointing to the beginning of the array (left) and another pointing to the end of the array (right).
+    Loop until left < right, and for each iteration
+        if arr[left] + arr[right] == target, then return the indices.
+        if arr[left] + arr[right] < target, increment the left index.
+        else, decrement the right index.
+
+This approach is called the two-pointer sliding window approach. It is a very common pattern for solving array related problems.
+
+Time complexity: O(n*log(n))
+*/
--- Java/Algorithms and Data Structures/String Algorithms/ReverseString.java
+++ Java/Algorithms and Data Structures/String Algorithms/ReverseString.java
@@ -1,4 +1,18 @@
 
 public class ReverseString {
-    
+    public static String reverseString(String args) {
+        char strLetter = (char)0;
+        StringBuilder sb = new StringBuilder();
+        String reverseOut = "";
+        for(int i= args.length() - 1;i >= 0;i--){
+            strLetter = args.charAt(i);
+            reverseOut = sb.append(strLetter).toString();
+        }
+        System.out.println(reverseOut);
+        return reverseOut;
+    }
+    public static void main(String[] args) {
+        String input = "lilac";
+        reverseString(input);
+    }
 }
