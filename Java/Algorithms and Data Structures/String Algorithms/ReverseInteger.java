d3fdd825e559e1d62732b7d1cc48ebdb1e9222fa
chintamanichavan <chintamani.chavan97@gmail.com>
2021-02-24 07:33:05 -0500

    ReverseInteger Java

--- Java/Algorithms and Data Structures/String Algorithms/ReverseInteger.java
+++ Java/Algorithms and Data Structures/String Algorithms/ReverseInteger.java
@@ -1,3 +1,16 @@
 public class ReverseInteger {
-    
+    public static int reverseInteger(int args) {
+        int reverseOut = 0,input = args,rem = 0;
+        while(input > 0){
+            rem = input % 10;
+            input = input / 10;
+            reverseOut = (reverseOut * 10) + rem;
+        }
+        System.out.println(reverseOut);
+        return reverseOut;
+    }
+    public static void main(String[] args) {
+        int input = 89;
+        reverseInteger(input);
+    }
 }
