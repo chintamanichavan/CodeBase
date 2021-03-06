8d2d2016be53b5d6adc48a2647a1ccf2f5d521a8
chintamanichavan <chintamani.chavan97@gmail.com>
2021-03-05 20:14:16 -0500

    Cleanup 03/05/2021

--- Java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Java;
-
-public class HelloWorld {
-    // Your First Program
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
-
--- Python/Collections/introCollections.py
+++
@@ -1,5 +0,0 @@
-# Collections: Counter, namedTuple, OrderDict, defaultdict, deque
-from collections import Counter
-a = "aaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbvvvvvvvvvvvvvvvvvvvvvvvvv"
-print(Counter(a))
-print(Counter(a).most_common(1))
